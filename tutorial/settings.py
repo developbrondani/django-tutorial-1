b84f0295fbfeb5c4212c2e7a7c11f61bed651d5d
Fernando Valente <fernandovalentedev@gmail.com>
2015-08-25 22:10:58 -0300

    atualizando o gitignore e configuranco python-decouple em settings.py

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a7tyt#@*+=of!dle!ube5nkae2@8=@d8a4^1y0w9(6^qen9&7g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
