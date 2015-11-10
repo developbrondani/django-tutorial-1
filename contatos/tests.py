0ff411ac6ecb49703230828b2267d68af445a8eb
Fernando Valente <fernandovalentedev@gmail.com>
2015-11-09 23:18:12 -0200

    retornando ao estado inicial dos arquivos contatos/tests.py e tutorial/urls.py

--- contatos/tests.py
+++ contatos/tests.py
@@ -2,15 +2,4 @@
 
 from django.test import TestCase
 
-
-class UrlsContatosTest(TestCase):
-
-    def setUp(self):
-        self.urls = ('/contatos/',)
-
-    def generate_test_url(self, url):
-        def test(self, url):
-            response = self.client.get(url)
-            message = '{} inaccessible'.format(url)
-            self.assertEquals(200, response.status_code, message)
-        return test(self, url)
+# Create your tests here.
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,5 +3,4 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^contatos/', include('contatos.urls', namespace='contatos')),
 ]
