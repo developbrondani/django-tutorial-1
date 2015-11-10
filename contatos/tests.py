23a3935a1a17be8ac68ea810518307ecebc1b5a5
Fernando Valente <fernandovalentedev@gmail.com>
2015-11-09 23:17:10 -0200

    atualização de contatos/tests, settungs e urls

--- contatos/tests.py
+++ contatos/tests.py
@@ -2,4 +2,15 @@
 
 from django.test import TestCase
 
-# Create your tests here.
+
+class UrlsContatosTest(TestCase):
+
+    def setUp(self):
+        self.urls = ('/contatos/',)
+
+    def generate_test_url(self, url):
+        def test(self, url):
+            response = self.client.get(url)
+            message = '{} inaccessible'.format(url)
+            self.assertEquals(200, response.status_code, message)
+        return test(self, url)
